684d6c8938e399b2b2110867cf7cf3f00a3ccf2e
kumapo <kumapo@users.noreply.github.com>
2022-08-28 14:40:46 +0900

    clean up preprocess

--- image_captioning/preprocess.py
+++ image_captioning/preprocess.py
@@ -23,21 +23,12 @@ def main(args: argparse.Namespace):
     print('preprocess: args=%s' % args.__dict__)
     save_args(args, args.output_dir / 'preprocess_args.json')
 
-    model = transformers.VisionEncoderDecoderModel.from_encoder_decoder_pretrained(
-        args.encoder_model_name_or_path, args.decoder_model_name_or_path
-    )
     feature_extractor = transformers.AutoFeatureExtractor.from_pretrained(args.encoder_model_name_or_path)
     tokenizer = transformers.AutoTokenizer.from_pretrained(args.decoder_model_name_or_path)
-    model.config.decoder_start_token_id = tokenizer.cls_token_id
-    model.config.pad_token_id = tokenizer.pad_token_id
 
     train_dataset = datasets.load_dataset(
         "kumapo/coco_dataset_script", "2017", 
         data_dir=str(args.train_data_dir), split="train", streaming=True
-    )
-    eval_dataset = datasets.load_dataset(
-        "kumapo/coco_dataset_script", "2017",
-        data_dir=str(args.valid_data_dir), split="validation", streaming=True
     )
     # https://github.com/huggingface/datasets/issues/4675
     def preprocess_function(examples):
@@ -63,13 +54,6 @@ def main(args: argparse.Namespace):
         batched=True,
         remove_columns=["image_path", "caption", 'image_id', 'width', 'file_name', 'coco_url', 'caption_id', 'height']
     )
-    eval_dataset = eval_dataset.map(
-        preprocess_function,
-        batched=True,
-        # batch_size=args.valid_batch_size,
-        # writer_batch_size=args.valid_batch_size,
-        remove_columns=["image_path", "caption", 'image_id', 'width', 'file_name', 'coco_url', 'caption_id', 'height']
-    )
     train_dataset = datasets.Dataset.from_dict(
         train_dataset._head(args.num_train_data),
         features=datasets.Features({
@@ -78,7 +62,7 @@ def main(args: argparse.Namespace):
         })
     ).with_format("torch")
     train_df = train_dataset = train_dataset.to_pandas()
-    train_df.to_csv(args.output_dir / 'train.csv')
+    train_df.to_csv(args.output_dir / 'preprocessed_train.csv')
 
     return
 
@@ -87,9 +71,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--train_data_dir", default='../input/coco-2017-train/', type=pathlib.Path
-    )
-    parser.add_argument(
-        "--valid_data_dir", default='../input/coco-2017-val/', type=pathlib.Path
     )
     parser.add_argument(
         "--output_dir", default=pathlib.Path('output'), type=pathlib.Path, help=""
@@ -108,9 +89,6 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--num_train_data", default=118287, type=int, help="number of items to train on dataset."
-    )
-    parser.add_argument(
-        "--num_valid_data", default=2000, type=int, help="number of items to evaluate on dataset."
     )
     parser.add_argument(
         "--debug", action="store_true",
