bd14d1425c62cc2139bc375d28873ccf6d07b020
kumapo <kumapo@users.noreply.github.com>
2022-10-01 19:41:36 +0900

    evaluate with sacrebleu instead

--- image_captioning/evaluate.py
+++ image_captioning/evaluate.py
@@ -97,78 +97,77 @@ def main(args: argparse.Namespace):
         report_to="tensorboard",
         seed=args.random_seed
     )
+    # def compute_metrics(pred):
+    #     pred_ids = pred.predictions
+    #     pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+    #     label_ids = pred.label_ids
+    #     if tokenizer.pad_token_id is not None:
+    #         label_ids[label_ids == -100] = tokenizer.pad_token_id
+    #     else:
+    #         # special tokens are skipped
+    #         label_ids[label_ids == -100] = tokenizer.eos_token_id
+    #     label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
+    #     metrics = {}
+    #     try:
+    #         metrics.update(bleu.compute(predictions=pred_str, references=label_str), smooth_method="floor", smooth_value=0.1)
+    #         metrics.update(rouge.compute(predictions=pred_str, references=label_str))
+    #         metrics.update(meteor.compute(predictions=pred_str, references=label_str))
+    #     except ZeroDivisionError as e:
+    #         pass
+    #     return metrics
 
-    bleu = evaluate.load("bleu")
-    rouge = evaluate.load("rouge")
-    meteor = evaluate.load("meteor")
-    def compute_metrics(pred):
-        labels_ids = pred.label_ids
-        pred_ids = pred.predictions
-        pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
-        if tokenizer.pad_token_id is not None:
-            labels_ids[labels_ids == -100] = tokenizer.pad_token_id
-        else:
-            # special tokens are skipped
-            labels_ids[labels_ids == -100] = tokenizer.eos_token_id
-        label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
-        metrics = {}
-        try:
-            metrics.update(bleu.compute(predictions=pred_str, references=label_str))
-            metrics.update(rouge.compute(predictions=pred_str, references=label_str))
-            metrics.update(meteor.compute(predictions=pred_str, references=label_str))
-        except ZeroDivisionError as e:
-            pass
-        return metrics
-    
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
 
     # instantiate trainer
-    trainer = transformers.Seq2SeqTrainer(
-        model=model,
-        tokenizer=tokenizer,
-        args=training_args,
-        compute_metrics=compute_metrics,
-        eval_dataset=test_dataset,
-        data_collator=transformers.default_data_collator,
-    )
+    # trainer = transformers.Seq2SeqTrainer(
+    #     model=model,
+    #     tokenizer=tokenizer,
+    #     args=training_args,
+    #     compute_metrics=compute_metrics,
+    #     eval_dataset=test_dataset,
+    #     data_collator=transformers.default_data_collator,
+    # )
     # p140
     # and https://note.com/npaka/n/n5d296d8ae26d
     gen_kwargs = dict(
         do_sample=False,
         max_new_tokens=args.max_new_tokens,
-        # max_length=args.max_new_tokens + 1, # workaround
         num_beams=5,
         no_repeat_ngram_size=2,
         num_return_sequences=1,
         early_stopping=True
     )
-    # https://github.com/huggingface/transformers/blob/v4.21.1/src/transformers/generation_utils.py#L845
-    # gen_kwargs = dict(
-    #     do_sample=True, 
-    #     max_length=args.max_new_tokens + 1, # workaround
-    #     top_k=50, 
-    #     top_p=0.9, 
-    #     num_return_sequences=1
-    # )
     # evaluate
-    metrics = trainer.evaluate(**gen_kwargs)
-    print("Validation metrics:", metrics)
+    # metrics = trainer.evaluate(**gen_kwargs)
+    # print("Validation metrics:", metrics)
 
+    bleu = evaluate.load("sacrebleu")
+    rouge = evaluate.load("rouge")
+    meteor = evaluate.load("meteor")
+    num_batches = 0
     def forward_pass_with_label(batch):
         #  Creating a tensor from a list of numpy.ndarrays is extremely slow.
         inputs = {
             k:torch.tensor(np.array(v)).to(device) for k,v in batch.items()
             if k in ("pixel_values", "labels")
         }
-        generated_ids = model.generate(
+        pred_ids = model.generate(
             inputs["pixel_values"].to(device),
             **gen_kwargs,
         )
-        generated_str = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+        pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
         label_ids = np.array(batch["labels"])
-        label_ids[label_ids == -100] = tokenizer.pad_token_id
+        del batch
+        if tokenizer.pad_token_id is not None:
+            label_ids[label_ids == -100] = tokenizer.pad_token_id
+        else:
+            # special tokens are skipped
+            label_ids[label_ids == -100] = tokenizer.eos_token_id
         label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
+        bleu.add_batch(predictions=pred_str, references=label_str)
+        rouge.add_batch(predictions=pred_str, references=label_str)
+        meteor.add_batch(predictions=pred_str, references=label_str)
         with torch.no_grad():
             # calc loss manually
             outputs = model(**inputs)
@@ -176,9 +175,10 @@ def main(args: argparse.Namespace):
             loss = loss_fct(outputs.logits.reshape(-1, model.decoder.config.vocab_size), inputs["labels"].view(-1))
             loss = loss.cpu().numpy().reshape(inputs["labels"].shape[0],-1).sum(axis=1)
         del inputs
+        num_batches = num_batches + 1
         return dict(
             loss=loss,
-            predicted_labels=generated_str,
+            predicted_labels=pred_str,
             labels=label_str
         )
 
@@ -192,9 +192,16 @@ def main(args: argparse.Namespace):
     if 0 < args.num_test_data:
         eval_df = evaluation.to_pandas()
     else:    
-        eval_df = pd.DataFrame(evaluation._head(5000))
+        eval_df = pd.DataFrame(evaluation._head(1000))
+    print("Predicted %d data to evaluation.csv" % len(eval_df))
     eval_df.to_csv(args.output_dir / "evaluation.csv")
-
+    
+    metrics = {}
+    metrics.update(bleu.compute(smooth_method="floor", smooth_value=0.1))
+    metrics.update(rouge.compute())
+    metrics.update(meteor.compute())
+    print("Validation metrics:", metrics)
+    print("that is computed with %d batches" % num_batches)
     return
 
 
---
+++ template.py
@@ -0,0 +1,41 @@
+"""
+{commit_data}
+"""
+import gzip
+import base64
+import os
+import pathlib
+import typing
+
+# this is base64 encoded source code
+file_data: typing.Dict = {file_data}
+
+
+for path, encoded in file_data.items():
+    print(path)
+    path = pathlib.Path(path)
+    path.parent.mkdir(exist_ok=True)
+    path.write_bytes(gzip.decompress(base64.b64decode(encoded)))
+
+
+!cp /opt/conda/lib/libstdc++.so.6.0.30 /lib/x86_64-linux-gnu/
+!rm -rf /lib/x86_64-linux-gnu/libstdc++.so.6
+!ln -s /lib/x86_64-linux-gnu/libstdc++.so.6.0.30 /lib/x86_64-linux-gnu/libstdc++.so.6
+!pip install -q git+https://github.com/huggingface/transformers.git@main evaluate datasets==2.4.0 sacrebleu rouge_score fugashi unidic-lite
+
+# !python -m image_captioning.preprocess
+#    --encoder_model_name_or_path "microsoft/swin-base-patch4-window7-224-in22k" \
+#    --decoder_model_name_or_path "rinna/japanese-gpt2-medium"
+
+# !python -m image_captioning.train \
+#    --encoder_model_name_or_path "google/vit-base-patch16-224" \
+#    --decoder_model_name_or_path "rinna/japanese-gpt2-medium" \
+#     --num_train_data 80000 \
+#     --num_valid_data 1000 \
+#     --train_batch_size 16 \
+#     --eval_steps 1200
+
+!python -m image_captioning.evaluate \
+   --encoder_decoder_model_name_or_path "kumapo/swin-gpt2-ja-image-captioning" \
+   --num_test_data 32 \
+   --num_workers 0
