58718f3f6baf2a156bea7872e052389ad883a0e2
kumapo <kumapo@users.noreply.github.com>
2022-10-15 17:48:37 +0900

    evaluate rouge with the same tokenizer as preprocess

--- image_captioning/evaluate.py
+++ image_captioning/evaluate.py
@@ -65,72 +65,65 @@ def main(args: argparse.Namespace):
                 for input_ids in encoded.input_ids
             ]
         return {
-            "id": image_ids,
             "pixel_values": pixel_values.squeeze(),
-            "labels": encoded.input_ids
+            "labels": encoded.input_ids,
+            "id": image_ids
         }
 
-    removed_columns = ["image_id","image_path","caption_id","caption","coco_url","file_name","height","width"]
+    preprocessed_features = datasets.Features({
+        "pixel_values": datasets.Array3D(shape=(3, 224, 224), dtype='float32'),
+        "labels": datasets.Sequence(feature=datasets.Value(dtype='int32'), length=args.max_sequence_length),
+        "id": datasets.Value(dtype='int64')
+    })
+    removed_columns = [
+        "image_id","image_path","caption_id","caption","coco_url","file_name","height","width"
+    ]
+    test_dataset = test_dataset.shuffle(
+        seed=args.random_seed,
+        buffer_size=10000
+    )
+    test_dataset = test_dataset.map(
+        preprocess_function,
+        batched=True,
+        remove_columns=removed_columns
+    ).cast(preprocessed_features)
+
     if 0 < args.num_test_data:
         test_dataset = datasets.Dataset.from_dict(
-            test_dataset.map(
-                preprocess_function,
-                batched=True,
-                remove_columns=removed_columns
-            )._head(args.num_test_data),
-            features=datasets.Features({
-                "id": datasets.Value(dtype='int64'),
-                "pixel_values": datasets.Array3D(shape=(3, 224, 224), dtype='float32'),
-                "labels": datasets.Sequence(feature=datasets.Value(dtype='int32'), length=args.max_sequence_length)
-            })
+            test_dataset._head(args.num_test_data),
+            features=preprocessed_features
         ).with_format("torch")
     else:
-        test_dataset = test_dataset.map(
-            preprocess_function,
-            batched=True,
-            remove_columns=removed_columns
-        ).with_format("torch")
+        test_dataset = test_dataset.with_format("torch")
 
-    # https://github.com/NielsRogge/Transformers-Tutorials/blob/master/TrOCR/Fine_tune_TrOCR_on_IAM_Handwriting_Database_using_Seq2SeqTrainer.ipynb
-    training_args = transformers.Seq2SeqTrainingArguments(
-        predict_with_generate=True,
-        per_device_eval_batch_size=args.test_batch_size,
-        fp16=False if args.debug else not args.no_fp16, 
-        output_dir=args.output_dir,
-        dataloader_num_workers=args.num_workers if not args.debug else 0,
-        report_to="tensorboard",
-        seed=args.random_seed
-    )
+    bleu = evaluate.load("sacrebleu")
+    rouge = evaluate.load("rouge")
+    meteor = evaluate.load("meteor")
+    def compute_metrics(pred):
+        label_ids = pred.label_ids
+        pred_ids = pred.predictions
+        pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+        if tokenizer.pad_token_id is not None:
+            label_ids[label_ids == -100] = tokenizer.pad_token_id
+        else:
+            # special tokens are skipped
+            label_ids[label_ids == -100] = tokenizer.eos_token_id
+        label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
+        metrics = {}
+        metrics.update(bleu.compute(
+            predictions=pred_str, references=label_str,
+            smooth_method="floor", smooth_value=0.1
+        ))
+        metrics.update(rouge.compute(
+            predictions=pred_str, references=label_str,
+            tokenizer=lambda x: tokenizer.tokenize(x)
+        ))
+        metrics.update(meteor.compute(predictions=pred_str, references=label_str))
+        return metrics
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
 
-    # def compute_metrics(pred):
-    #     pred_ids = pred.predictions
-    #     pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
-    #     label_ids = pred.label_ids
-    #     if tokenizer.pad_token_id is not None:
-    #         label_ids[label_ids == -100] = tokenizer.pad_token_id
-    #     else:
-    #         # special tokens are skipped
-    #         label_ids[label_ids == -100] = tokenizer.eos_token_id
-    #     label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
-    #     metrics = {}
-    #     try:
-    #         metrics.update(bleu.compute(predictions=pred_str, references=label_str), smooth_method="floor", smooth_value=0.1)
-    #         metrics.update(rouge.compute(predictions=pred_str, references=label_str))
-    #         metrics.update(meteor.compute(predictions=pred_str, references=label_str))
-    #     except ZeroDivisionError as e:
-    #         pass
-    #     return metrics
-    # 
-    # trainer = transformers.Seq2SeqTrainer(
-    #     model=model,
-    #     tokenizer=tokenizer,
-    #     args=training_args,
-    #     compute_metrics=compute_metrics,
-    #     eval_dataset=test_dataset,
-    #     data_collator=transformers.default_data_collator,
-    # )
     # p140
     # and https://note.com/npaka/n/n5d296d8ae26d
     gen_kwargs = dict(
@@ -141,73 +134,91 @@ def main(args: argparse.Namespace):
         num_return_sequences=1,
         early_stopping=True
     )
-    # evaluate
-    # metrics = trainer.evaluate(**gen_kwargs)
-    # print("Validation metrics:", metrics)
-
-    bleu = evaluate.load("sacrebleu")
-    rouge = evaluate.load("rouge")
-    meteor = evaluate.load("meteor")
-    def forward_pass_with_label(batch):
-        #  Creating a tensor from a list of numpy.ndarrays is extremely slow.
-        inputs = {
-            k:torch.tensor(np.array(v)).to(device) for k,v in batch.items()
-            if k in ("pixel_values", "labels")
-        }
-        pred_ids = model.generate(
-            inputs["pixel_values"].to(device),
-            **gen_kwargs,
-        )
-        pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
-        label_ids = np.array(batch["labels"])
-        if tokenizer.pad_token_id is not None:
-            label_ids[label_ids == -100] = tokenizer.pad_token_id
-        else:
-            # special tokens are going to be skipped
-            label_ids[label_ids == -100] = tokenizer.eos_token_id
-        label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
-        bleu.add_batch(predictions=pred_str, references=label_str)
-        rouge.add_batch(predictions=pred_str, references=label_str)
-        meteor.add_batch(predictions=pred_str, references=label_str)
-        with torch.no_grad():
-            # calc loss manually
-            outputs = model(**inputs)
-            loss_fct = torch.nn.CrossEntropyLoss(reduction="none")
-            loss = loss_fct(outputs.logits.reshape(-1, model.decoder.config.vocab_size), inputs["labels"].view(-1))
-            loss = loss.cpu().numpy().reshape(inputs["labels"].shape[0],-1).sum(axis=1)
-        ids = np.array(batch["id"]).astype(int) # the type would be either list or tensor
-        del batch, inputs
-        return dict(
-            id=ids,
-            loss=loss,
-            predicted_labels=pred_str,
-            labels=label_str
-        )
-
-    evaluation = test_dataset.map(
-        forward_pass_with_label,
-        batched=True,
-        batch_size=args.test_batch_size,
-        remove_columns=["pixel_values"],
-        drop_last_batch=False
+    # https://github.com/NielsRogge/Transformers-Tutorials/blob/master/TrOCR/Fine_tune_TrOCR_on_IAM_Handwriting_Database_using_Seq2SeqTrainer.ipynb
+    training_args = transformers.Seq2SeqTrainingArguments(
+        predict_with_generate=True,
+        per_device_eval_batch_size=args.test_batch_size,
+        fp16=False if args.debug else not args.no_fp16, 
+        output_dir=args.output_dir,
+        dataloader_num_workers=args.num_workers if not args.debug else 0,
+        report_to="tensorboard",
+        seed=args.random_seed
     )
-    if 0 < args.num_test_data:
-        # we cannot use DataFrame():
-        # https://github.com/huggingface/datasets/issues/469
-        eval_df = evaluation.to_pandas()
-    else:
-        eval_df = pd.concat([
-            pd.DataFrame(_dict, index=[_dict["id"]])
-            for _dict in evaluation
-        ])
-    print("Predicted %d data to evaluation.csv" % len(eval_df))
-    eval_df.to_csv(args.output_dir / "evaluation.csv")
-    
-    metrics = {}
-    metrics.update(bleu.compute(smooth_method="floor", smooth_value=0.1, tokenize='ja-mecab'))
-    metrics.update(rouge.compute(tokenizer=lambda x: tokenizer.tokenize(x)))
-    metrics.update(meteor.compute())
+    trainer = transformers.Seq2SeqTrainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=training_args,
+        compute_metrics=compute_metrics,
+        eval_dataset=test_dataset,
+        data_collator=transformers.default_data_collator,
+    )
+    # evaluate
+    metrics = trainer.evaluate(**gen_kwargs)
     print("Validation metrics:", metrics)
+
+    # bleu = evaluate.load("sacrebleu")
+    # rouge = evaluate.load("rouge")
+    # meteor = evaluate.load("meteor")
+    # def forward_pass_with_label(batch):
+    #     #  Creating a tensor from a list of numpy.ndarrays is extremely slow.
+    #     inputs = {
+    #         k:torch.tensor(np.array(v)).to(device) for k,v in batch.items()
+    #         if k in ("pixel_values", "labels")
+    #     }
+    #     pred_ids = model.generate(
+    #         inputs["pixel_values"].to(device),
+    #         **gen_kwargs,
+    #     )
+    #     pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+    #     label_ids = np.array(batch["labels"])
+    #     if tokenizer.pad_token_id is not None:
+    #         label_ids[label_ids == -100] = tokenizer.pad_token_id
+    #     else:
+    #         # special tokens are going to be skipped
+    #         label_ids[label_ids == -100] = tokenizer.eos_token_id
+    #     label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
+    #     bleu.add_batch(predictions=pred_str, references=label_str)
+    #     rouge.add_batch(predictions=pred_str, references=label_str)
+    #     meteor.add_batch(predictions=pred_str, references=label_str)
+    #     with torch.no_grad():
+    #         # calc loss manually
+    #         outputs = model(**inputs)
+    #         loss_fct = torch.nn.CrossEntropyLoss(reduction="none")
+    #         loss = loss_fct(outputs.logits.reshape(-1, model.decoder.config.vocab_size), inputs["labels"].view(-1))
+    #         loss = loss.cpu().numpy().reshape(inputs["labels"].shape[0],-1).sum(axis=1)
+    #     ids = np.array(batch["id"]).astype(int) # the type would be either list or tensor
+    #     del batch, inputs
+    #     return dict(
+    #         id=ids,
+    #         loss=loss,
+    #         predicted_labels=pred_str,
+    #         labels=label_str
+    #     )
+
+    # evaluation = test_dataset.map(
+    #     forward_pass_with_label,
+    #     batched=True,
+    #     batch_size=args.test_batch_size,
+    #     remove_columns=["pixel_values"],
+    #     drop_last_batch=False
+    # )
+    # if 0 < args.num_test_data:
+    #     # we cannot use DataFrame():
+    #     # https://github.com/huggingface/datasets/issues/469
+    #     eval_df = evaluation.to_pandas()
+    # else:
+    #     eval_df = pd.concat([
+    #         pd.DataFrame(_dict, index=[_dict["id"]])
+    #         for _dict in evaluation
+    #     ])
+    # print("Predicted %d data to evaluation.csv" % len(eval_df))
+    # eval_df.to_csv(args.output_dir / "evaluation.csv")
+    
+    # metrics = {}
+    # metrics.update(bleu.compute(smooth_method="floor", smooth_value=0.1, tokenize='ja-mecab'))
+    # metrics.update(rouge.compute(tokenizer=lambda x: tokenizer.tokenize(x)))
+    # metrics.update(meteor.compute())
+    # print("Validation metrics:", metrics)
     return
 
 
